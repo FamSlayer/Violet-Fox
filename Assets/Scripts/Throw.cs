8341da8d14554facd0d9f30589c36b089dafef4b
FamSlayer <fuller.rules@gmail.com>
2016-11-18 03:30:18 -0500

    Made the pre-lim project with a .gitignore in the right place

---
+++ Assets/Scripts/Throw.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Throw : MonoBehaviour
+{
+
+    public GameObject held_item_;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        print(held_item_);
+	}
+}
