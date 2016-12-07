cd02b252136fcc76096b21a2977e89f1175b9e55
BeigeSand <aucoin.corwin@gmail.com>
2016-12-07 03:31:19 -0800

    Pushing shit

---
+++ Assets/Scripts/WinScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class WinScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+    }
+	void activated()
+    {
+        SceneManager.LoadScene("MainScene", LoadSceneMode.Additive);
+    }
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            activated();
+        }
+    }
+}
